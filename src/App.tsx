dc01a02217770b59988207b5d58fdf3e566972db
ale <ale@ale.com>
2022-08-15 19:38:49 -0300

    build: building project within dependencies

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Heading as="h1" fontSize="2xl">
+      Hamburgueria
+    </Heading>
+  );
+};
+
+export default App;
