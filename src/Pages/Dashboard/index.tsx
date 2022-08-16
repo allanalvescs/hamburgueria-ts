2f1fad71340f2f23dd9d6bdc6cc6fd58f441a5fa
ale <ale@ale.com>
2022-08-16 19:28:15 -0300

    feat: created Register page

---
+++ src/Pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Button, Heading } from "@chakra-ui/react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Heading>Dashboard</Heading>
+      <Button>Dewslogar</Button>
+    </>
+  );
+};
+
+export default Dashboard;
